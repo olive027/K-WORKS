f40f7b8d73449ec4898978c2c615cea56bcc093e
olive027 <blueblueww@yahoo.co.jp>
2023-11-18 05:29:20 +0900

    231118

---
+++ src/js/script.js
@@ -0,0 +1,55 @@
+
+jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
+	// swiper
+	// const swiper = new Swiper(".swiper1", {
+	// 	loop: true,
+	// 	autoplay: {
+	// 		delay: 2500,
+	// 		disableOnInteraction: false,
+	// 	},
+	// 	effect: 'fade',
+	// 	speed: 2500,
+	// 	allowTouchMove: false,
+	// });
+
+	// recruitのswiper
+	const swiper = new Swiper(".swiper", {
+		loop: true, // ループ有効
+		slidesPerView: 2, // 一度に表示する枚数
+		spaceBetween: 10,  //カード間のpx
+		breakpoints: {
+			// スライドの表示枚数：767px以上の場合
+			767: {
+				slidesPerView: 3,
+			}
+		},
+		speed: 4000, // ループの時間
+		allowTouchMove: false, // スワイプ無効
+		autoplay: {
+			delay: 0, // 途切れなくループ
+			pauseOnMouseEnter: true,
+      disableOnInteraction: false,
+		},
+	});
+
+	// headerのスクロールが止まったら背景色
+let timeout;
+$(window).scroll(function(){
+	$(".header").addClass('active');
+
+	clearTimeout(timeout);
+
+	timeout = setTimeout(function(){
+		$(".header").removeClass('active');
+	},300);
+});
+
+// hamburger
+$('.js-hamburger').click(function(){
+	$('.js-hamburger').toggleClass('is-active');
+	// $('.js-sp-nav').fadeToggle(600);
+	$('html').toggleClass('is-fixed');　//ハンバーガー展開時に背景を固定
+	$('.js-sp-nav').toggleClass('is-active'); //右からメニューが出てくる等の場合はこっちの記述
+})
+
+});
